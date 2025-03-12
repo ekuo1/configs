b1a0e10dc5db363a80ab19ade0c5a622be0c06e2
Turtle <Emilykuo5@hotmail.com>
2025-03-12 14:42:29 +1100

    t

--- home.nix
+++ home.nix
@@ -16,29 +16,8 @@
 
   programs.vim = {
     enable = true;
-
-    # Add your custom vim settings here
-    extraConfig = ''
-      set smartindent
-      set tabstop=2
-      set expandtab
-      set shiftwidth=2
-
-      " Color scheme
-      color koehler
-
-      " Bell settings
-      set visualbell
-      set t_vb=
-      set vb t_vb=
-      set noerrorbells
-      set belloff=all
-
-      " Paste toggle
-      set pastetoggle=<F3>
-    '';
   };
 
   # Set the Home Manager state version
-  home.stateVersion = "23.11";  # Adjust to your NixOS version
+  home.stateVersion = "24.11";  # Adjust to your NixOS version
 }
