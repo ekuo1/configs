ef5e8c1d7fcdbadc7b97a42eee657ff5d20b91d9
Turtle <Emilykuo5@hotmail.com>
2025-03-12 14:51:50 +1100

    home

--- home.nix
+++ home.nix
@@ -6,12 +6,36 @@
 
   # List of packages to install for this user
   home.packages = with pkgs; [
-    zsh
     tmux
   ];
 
   # Enable Zsh
   programs.zsh.enable = true;
+
+  programs.vim = {
+    enable = true;
+
+    # Add your custom vim settings here
+    extraConfig = ''
+      set smartindent
+      set tabstop=2
+      set expandtab
+      set shiftwidth=2
+
+      " Color scheme
+      color koehler
+
+      " Bell settings
+      set visualbell
+      set t_vb=
+      set vb t_vb=
+      set noerrorbells
+      set belloff=all
+
+      " Paste toggle
+      set pastetoggle=<F3>
+    '';
+  };
 
   # Set the Home Manager state version
   home.stateVersion = "24.11";  # Adjust to your NixOS version
