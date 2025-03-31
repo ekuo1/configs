6a5eb42983f6063ecb7fb3e988369c1fd621dbbe
Turtle <Emilykuo5@hotmail.com>
2025-03-31 14:34:25 +1100

    fix typo

--- home.nix
+++ home.nix
@@ -53,7 +53,7 @@ in {
   };
 
   programs.bash= {
-    enable = truel
+    enable = true;
     bashrcExtra = ''
       alias nixupdate='cd /etc/nixos && sudo git fetch origin main && sudo git reset origin/main --hard && nixos-rebuild switch'
       alias starthugo='cd ~'
