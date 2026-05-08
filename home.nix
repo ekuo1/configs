924f5f4600eaaa4765ed69d1c0b30453b3b57d52
Turtle <Emilykuo5@hotmail.com>
2026-05-08 14:25:02 +1000

    Allow unfree packages in Nix configuration

--- home.nix
+++ home.nix
@@ -11,6 +11,9 @@ in {
   home.username = "nixos";
   home.homeDirectory = "/home/nixos";
 
+  # packer is not free
+  nixpkgs.config.allowUnfree = true;
+
   # List of packages to install for this user
   home.packages = [
     pkgs.tmux
